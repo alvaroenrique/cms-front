66b1a15a590953e6ea23bf6cf59eecbdc32125ec
Alvaro <ahuamani@rmgperustaff.com>
2019-10-01 23:17:02 -0500

    [update] vista admin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import styled from "styled-components";
 
 import UiHeader from "./components/ui/header/default";
 import UiFooter from "./components/ui/footer/default";
@@ -24,18 +25,71 @@ const pages = [
   }
 ];
 
+const AdminView = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  max-width: 300px;
+  width: 100%;
+  height: 100vh;
+  padding: 20px;
+  position: fixed;
+  background-color: #e4e4e4;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  margin-left: 300px;
+`;
+
+const H1 = styled.h1`
+  font-weight: 700;
+  font-size: 2rem;
+  padding-bottom: 20px;
+  color: #4c4c4c;
+`;
+
+const H2 = styled.h2`
+  font-weight: 700;
+  font-size: 1.5rem;
+  margin: 20px 0;
+`;
+
+const PagesLinks = styled(Link)`
+  color: #333;
+  text-decoration: none;
+  padding: 5px 0;
+  display: block;
+  border-bottom: 1px solid gray;
+`;
+
 function App() {
   return (
     <BrowserRouter>
-      <UiHeader></UiHeader>
+      <AdminView>
+        <Left>
+          <H1>Edición de páginas</H1>
+          <H2>Páginas</H2>
+          {pages.map(page => (
+            <div>
+              <PagesLinks to={page.path}>{page.path}</PagesLinks>
+            </div>
+          ))}
+          <H2>Componentes</H2>
+        </Left>
+        <Right>
+          <UiHeader></UiHeader>
 
-      <Switch>
-        {pages.map(page => (
-          <Route {...page}></Route>
-        ))}
-      </Switch>
+          <Switch>
+            {pages.map(page => (
+              <Route {...page}></Route>
+            ))}
+          </Switch>
 
-      <UiFooter></UiFooter>
+          <UiFooter></UiFooter>
+        </Right>
+      </AdminView>
     </BrowserRouter>
   );
 }
